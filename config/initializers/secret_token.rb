5ab402bf768a8555bebfc175381d9fbc34edcf1e
OneBinaryZero <one.binary.zero@outlook.com>
2014-03-02 21:12:35 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '9512834cd4baf914802a8acfd1dafeca0f74487527d6952130c60da14017a7b6dda8b143d2997d441de6e18df47fa628d9ae401760eb8c062aff0d70827cc519'
